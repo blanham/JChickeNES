bbe5fd607cdb09626847d5086763d4bc9e40bd2a
Bryce Lanham <blanham@gmail.com>
2012-02-18 19:39:25 -0600

    Added a few instructions

--- src/com/pack/chickenes/JMOS6502.java
+++ src/com/pack/chickenes/JMOS6502.java
@@ -48,7 +48,7 @@ public class JMOS6502 {
 		out.printf("%4X ", pc & 0xFFFF);
 		switch(len){
 			case 1:
-				out.printf("%X02       ", read_ram(pc));
+				out.printf("%02X       ", read_ram(pc));
 				break;
 			case 2:
 				out.printf("%02X %02X    ", read_ram(pc), read_ram(pc+1));
@@ -57,14 +57,25 @@ public class JMOS6502 {
 				out.printf("%02X %02X %02X ", read_ram(pc),read_ram(pc+1),read_ram(pc+2));
 				break;
 		}
-		out.printf("%-12s", op);
+		out.printf("%-16s", op);
 		out.printf("A:%02X X:%02X Y:%02X P:%02X SP:%02X CYC: %3d\n", a,x,y,flags,sp, tmpCycles);
 	}
-	private void print_absolute(String name){
+	private void printAbsolute(String name){
 		print_stats(String.format("%s $%02X%02X", name, read_ram(pc+2),read_ram(pc+1)),3);
+	}
+	private void printBranch(String name){
+		print_stats(String.format("%s $%04X", name, (pc + read_ram(pc+1) + 2) & 0xFFFF),2);
+	}
+	private void printImplied(String name){
+		print_stats(String.format("%s", name),1);
+
 	}
 	private void printImmediate(String name){
 		print_stats(String.format("%s #$%02X", name, read_ram(pc+1)),2);
+	}
+	private void printZeroPage(String name, byte value){
+		print_stats(String.format("%s #$%02X = %02x", name, read_ram(pc+1), value),2);
+
 	}
 	private void checkFlags(int val)
 	{
@@ -79,35 +90,84 @@ public class JMOS6502 {
 			flags &= ~0x80;
 		}
 	}
+	private void pushPC(){
+		pc += 2;
+		ram[0x100 + sp--] = (byte)((pc >> 8) & 0xFF);
+		ram[0x100 + sp--] = (byte)(pc & 0xFF);
+		pc -= 2;
+	}
 	private void doJumps(int op)
 	{
 		switch(op)
 		{
 			case 0x4C:
-				print_absolute("JMP");
 				cycles += 3;
 				pc =   (read_ram(pc+1) & 0xFF) + (read_ram(pc+2) << 8);
 				break;
+			case 0x20://JSR
+				pushPC();
+				pc =   (read_ram(pc+1) & 0xFF) + (read_ram(pc+2) << 8);
+				cycles += 6;
+				break;
+		}
+	}
+	private void doFlags(int op){
+		switch(op){
+		case 0x18: flags &= ~0x1; pc++; cycles += 2; break;
+		case 0x38: flags |= 0x1; pc++; cycles += 2; break;
+		}
+	}
+	private void doBranch(boolean condition){
+		cycles += 2;
+		if(condition){
+			System.out.println("BRANCH");
+			int savepc = pc + (char)read_ram(pc + 1);
+			pc = savepc;
+			cycles += 1;
+		}
+			
+		pc += 2;
+	}
+	public void logger(int op){
+		switch(op){
+		//Branches
+		case 0x90: printBranch("BCC"); break;
+		case 0xB0: printBranch("BCS"); break;
+		//Flags
+		case 0x18: printImplied("CLC"); break;
+		case 0x38: printImplied("SEC"); break;
+		//JMPs
+		case 0x20: printAbsolute("JSR"); break;
+		case 0x4C: printAbsolute("JMP"); break;
+		//LDX
+		case 0xA2: printImmediate("LDX"); break;
+		//NOP
+		case 0xEA: printImplied("NOP"); break;
+		//STX
+		case 0x86: printZeroPage("STX", x); break;
+
 		}
 	}
 	public void do_op(){
-		int op = read_ram(pc);
-		switch(op & 0xFF)
+		int op = read_ram(pc) & 0xFF;
+		logger(op);
+		switch(op)
 		{
-		case 0x4C:
+		//Branches
+		case 0x90: doBranch((flags & 0x1) == 0);
+		case 0xB0: doBranch((flags & 0x1) != 0);
+		//Flags
+		case 0x18: case 0x38: doFlags(op);
+		//Jumps
+		case 0x20: case 0x4C:
 			doJumps(op);
 			break;
-		case 0x78:
-			System.out.println("SEI");
-			pc++;
-			break;
-		case 0xA2:
-			printImmediate("LDX");
-			x = read_ram(pc + 1);
-			checkFlags(x);
-			cycles += 2;
-			pc += 2;
-			break;
+		//LDX
+		case 0xA2: x = readImmediate(); checkFlags(x); break;
+		//NOP
+		case 0xEA: cycles += 2; pc++; break;
+		//STX
+		case 0x86: writeZeroPage(x); break;
 		default:
 			//System.out.println("Unimplemented opcode: " + Integer.toHexString(op&0xFF)
 				//	+ " @ " + Integer.toHexString(pc&0xFFFF) );
@@ -115,5 +175,15 @@ public class JMOS6502 {
 			running = false;				
 		
 		}
+	}
+	private byte readImmediate(){
+		cycles += 2;
+		pc+=2;
+		return read_ram(pc+1);
+	}
+	private void writeZeroPage(byte value){
+		write_ram(read_ram(pc+1) & 0xFF, value);
+		cycles += 3;
+		pc += 2;
 	}
 }
